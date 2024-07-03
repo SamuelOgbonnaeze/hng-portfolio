398889864a98dbdd10c3076316f903deb0b421b7
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2024-07-03 02:19:35 +0100

    UTC time update

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-function getCurrentTimeAndDay() {
-    const now = new Date();
-  
-    // Get the current time
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-  
-    const currentTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
-  
-    // Get the current day
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const currentDay = days[now.getDay()];
-  
-    return { hours, currentTime, currentDay };
-  }
-  
-  function Home() {
-    const { hours, currentTime, currentDay } = getCurrentTimeAndDay();
-  
-    const timeMessage = document.getElementById('time-message');
-    const DayMessage= document.getElementById('day-message');
+function getCurrentTime() {
+  const now = new Date();
 
-    DayMessage.innerHTML= `Today is ${currentDay}.`
+  // Get the current time in UTC
+  const hours = now.getUTCHours();
+  const minutes = now.getUTCMinutes();
 
-    timeMessage.innerHTML = `The time is ${currentTime}. ${hours >= 18 ? 'How did your day go?' : 'How is your day going?'}`;
-  }
-  
-  document.addEventListener('DOMContentLoaded', Home);
-  
+  const currentTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+
+  return { hours, currentTime };
+}
+
+function getCurrentDay() {
+  const now = new Date();
+
+  // Get the current day in UTC
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const currentDay = days[now.getUTCDay()];
+
+  return { currentDay };
+}
+
+function Home() {
+  const { hours, currentTime } = getCurrentTime();
+  const { currentDay } = getCurrentDay();
+
+  const timeMessage = document.getElementById('time-message');
+  const dayMessage = document.getElementById('day-message');
+
+  dayMessage.innerHTML = `Today is ${currentDay}.`;
+  timeMessage.innerHTML = `The time is ${currentTime}. ${hours >= 18 ? 'How did your day go?' : 'How is your day going?'}`;
+}
+
+document.addEventListener('DOMContentLoaded', Home);
